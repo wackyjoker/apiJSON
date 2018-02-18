2ef57c63a6a0c36c04d8553f2a72c229c6bc3cc5
wackyjoker <kevin.young8931@gmail.com>
2018-02-18 18:13:29 +0100

    updated some actions

--- public/app.js
+++ public/app.js
@@ -12,8 +12,15 @@ $(document).ready(function(){
 			createTodo();
 		}
 	});
-	$('.list').on('click','span',function(){
-		console.log('clicked');
+
+	$('.list').on('click','li',function(){
+		updateTodo($(this));
+	});
+
+
+	$('.list').on('click','span',function(event){
+		event.stopPropagation();
+		removeTodo($(this).parent());
 	});
 });
 
@@ -28,6 +35,8 @@ function addTodos(todos){
 
 function addTodo(todo){
 	const newTodo = $(`<li class="task">${todo.name}<span>X</span></li>`);
+	newTodo.data('id',todo._id);
+	newTodo.data('completed',todo.completed);
 		if(todo.completed){
 			newTodo.addClass("done");
 		}
@@ -44,5 +53,39 @@ function createTodo(){
 	})
 	.catch(function(err){
 		res.send(err);
+	})
+}
+
+function removeTodo(todo){
+	const clickedId = todo.data('id');
+	const deleteUrl = `/api/todo/${clickedId}`;
+	$.ajax({
+		method:'DELETE',
+		url:deleteUrl
+	})
+	.then(function(data){
+		todo.remove();
+	})
+	.catch(function(err){
+		console.log(err);
+	})
+}
+
+
+function updateTodo(todo){
+	const updateUrl = `/api/todo/${todo.data('id')}`;
+	const isDone = !todo.data('completed');
+	const updateData = {completed:isDone}
+	$.ajax({
+		method : 'PUT',
+		url:updateUrl,
+		data:updateData
+	})
+	.then(function(updatedTodo){
+		todo.toggleClass('done');
+		todo.data('completed',isDone);
+	})
+	.catch(function(err){
+		console.log(err);
 	})
 }
